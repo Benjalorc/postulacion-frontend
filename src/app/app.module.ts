59874a606dfa8c78ed79bd3d20c16efeaf66fc96
Benjaseg96 <benjamin.s1.e@gmail.com>
2020-05-07 03:01:53 -0400

    Ejecución de consulta al API y captura de resultados
    Visualización de datos obtenidos en vista por columnas
    
    Asignación de imagen a cada servicio del api
    
    Animacion de carga + Mensajes de estado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BusquedaApiComponent } from './components/busqueda-api/busqueda-api.component';
+
 
 @NgModule({
   declarations: [
@@ -13,6 +15,7 @@ import { BusquedaApiComponent } from './components/busqueda-api/busqueda-api.com
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     HttpClientModule,
     AppRoutingModule
   ],
--- src/app/components/busqueda-api/busqueda-api.component.ts
+++ src/app/components/busqueda-api/busqueda-api.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GetApiService } from '../../services/get-api/get-api.service';
 
 @Component({
   selector: 'app-busqueda-api',
@@ -7,9 +8,72 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BusquedaApiComponent implements OnInit {
 
-  constructor() { }
+	media: any;
+	datos: {
+		media: {
+			param: string;
+			data: any;
+			err: {
+				title: string,
+				text: string
+			};
+		}
+	};
 
-  ngOnInit() {
-  }
+	loading: boolean;
 
+	constructor(public getApi: GetApiService) { }
+
+	ngOnInit() {
+		this.datos = {
+			media: {
+				param: "",
+				data: null,
+				err: null
+			},
+		};
+		this.loading = false;
+	}
+
+	buscarPorNombre(nombre){
+		this.loading = true;
+		this.datos.media.data = [];
+		this.getApi.getMedia(nombre).subscribe(data=>{
+
+			this.asignarImagen(data);
+
+			this.datos.media.data = data;
+			this.datos.media.err = null;
+			this.loading = false;
+		},
+		err=>{
+			setTimeout(()=>{
+				this.datos.media.data = [];
+				this.loading = false;
+				this.datos.media.err = {
+					title: "No se pudo conectar con el API.",
+					text: "Verifique su conexión a internet y vuelva a intentar."
+				};
+			},1000);
+		});
+	}
+
+	asignarImagen(arr){
+
+		let images = [
+			"assets/images/debian.png",
+			"assets/images/lubuntu.png",
+			"assets/images/mint.png"
+		];
+
+		let servicios = [];
+		for(let i = 0, j = arr.length; i<j; i++){
+
+			let origen = arr[i].origen;
+			if(!servicios.find((el)=> el==origen)) servicios.push(origen);
+
+			let index = servicios.findIndex((el)=> el == origen);
+			arr[i].imagen = images[index];
+		}
+	}
 }
